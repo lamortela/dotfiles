58f2745d323aec39d7e4a30fea29b01c652885a9
Anthony Lamorte <lamorte@gmail.com>
2015-05-17 11:52:23 -0400

    Updated brew install script with new packages

--- brew.sh
+++ brew.sh
@@ -17,8 +17,12 @@ brew install tmux
 brew install tree
 
 brew install python
+brew install postgresql
 brew install sqlite
 brew install nvm
+
+brew install ctags
+brew install qt
 
 brew cleanup
 
