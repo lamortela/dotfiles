db3e05bf9d1eb560c2ad01ac4a7fc122e1a9da89
Anthony Lamorte <lamorte@gmail.com>
2016-09-20 21:22:38 -0400

    Have bootstrap script skip git pull if changes found

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,24 +2,34 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin master;
+gitPull() {
+  local status=$(git status -b --porcelain 2> /dev/null || git status --porcelain 2> /dev/null)
 
-function doIt() {
-    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+  if [[ -z "${status}" ]]; then
+    git pull origin master;
+  else
+    echo "Changes found, skipping git pull."
+  fi;
+}
+
+doIt() {
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
         --exclude "README.md" --exclude "LICENSE.md" --exclude "sublime/" \
-	-avh --no-perms . ~;
-    source ~/.bash_profile;
-
-    #TODO: If sublime app support directory exists, rsync sublime user settings
+	      -avh --no-perms . ~;
+  #TODO: If sublime app support directory exists, rsync sublime user settings
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-    doIt;
+  gitPull;
+  doIt;
 else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-    echo "";
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        doIt;
-    fi;
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+  echo "";
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    gitPull;
+    doIt;
+  fi;
 fi;
+
+unset gitPull;
 unset doIt;
