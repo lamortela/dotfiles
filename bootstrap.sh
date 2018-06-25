2bcc552e723f790c449c800b69046252fe6ba925
Anthony Lamorte <lamorte@gmail.com>
2018-06-25 10:32:33 -0400

    Get rid of git pull in bootstrap script
    It's never been needed, and it makes this script do too much.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,16 +2,6 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-gitPull() {
-  local status=$(git status -b --porcelain 2> /dev/null || git status --porcelain 2> /dev/null)
-
-  if [[ -z "${status}" ]]; then
-    git pull origin master;
-  else
-    echo "Changes found, skipping git pull."
-  fi;
-}
-
 doIt() {
   rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
         --exclude "README.md" --exclude "LICENSE.md" --exclude "sublime/" \
@@ -28,18 +18,15 @@ if [ "$1" == "--dry-run" ]; then
   pretendToDoIt;
 else
   if [ "$1" == "--force" -o "$1" == "-f" ]; then
-    gitPull;
     doIt;
   else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    read -p "This will overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
     echo "";
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-      gitPull;
       doIt;
     fi;
   fi;
 fi;
 
-unset gitPull;
 unset doIt;
 unset pretendToDoIt;
