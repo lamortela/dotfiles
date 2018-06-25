079eb6aadfef6935ab3626c795ad62484a0e0baf
Anthony Lamorte <lamorte@gmail.com>
2018-06-25 10:30:28 -0400

    Add dry-run argument to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,20 +16,30 @@ doIt() {
   rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
         --exclude "README.md" --exclude "LICENSE.md" --exclude "sublime/" \
 	      -avh --no-perms . ~;
-  #TODO: If sublime app support directory exists, rsync sublime user settings
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  gitPull;
-  doIt;
+pretendToDoIt() {
+  rsync --dry-run --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+        --exclude "README.md" --exclude "LICENSE.md" --exclude "sublime/" \
+	      -avh --no-perms . ~;
+}
+
+if [ "$1" == "--dry-run" ]; then
+  pretendToDoIt;
 else
-  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-  echo "";
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
+  if [ "$1" == "--force" -o "$1" == "-f" ]; then
     gitPull;
     doIt;
+  else
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      gitPull;
+      doIt;
+    fi;
   fi;
 fi;
 
 unset gitPull;
 unset doIt;
+unset pretendToDoIt;
